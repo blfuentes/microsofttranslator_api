077d002dc307b7556c2783c38369dad3146a2bdb
blfuentes <epiphonetodie@gmail.com>
2018-09-29 17:19:55 +0200

    added gitignore, debug launch config for vscode and read from settings

---
+++ main.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, uuid, json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# READ subscription key from config file
+with open('settings.json', 'r') as file:
+    tmpSettings = json.load(file)
+
+# Replace the subscriptionKey string value with your valid subscription key.
+subscriptionKey = tmpSettings["SUBSCRIPTION_KEY"]
+
+host = 'api.cognitive.microsofttranslator.com'
+path = '/translate?api-version=3.0'
+
+# Translate to German and Italian.
+params = "&to=de&to=it";
+
+text = 'Hello, world!'
+
+def translate (content):
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscriptionKey,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    conn = http.client.HTTPSConnection(host)
+    conn.request ("POST", path + params, content, headers)
+    response = conn.getresponse ()
+    return response.read ()
+
+requestBody = [{
+    'Text' : text,
+}]
+content = json.dumps(requestBody, ensure_ascii=False).encode('utf-8')
+result = translate (content)
+
+# Note: We convert result, which is JSON, to and from an object so we can pretty-print it.
+# We want to avoid escaping any Unicode characters that result contains. See:
+# https://stackoverflow.com/questions/18337407/saving-utf-8-texts-in-json-dumps-as-utf8-not-as-u-escape-sequence
+output = json.dumps(json.loads(result), indent=4, ensure_ascii=False)
+
+print (output)
